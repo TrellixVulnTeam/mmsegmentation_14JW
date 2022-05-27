c18408418a8692d372000336d8a34ca883693875
Shiweiliuiiiiiii <s.liu3@tue.nl>
2022-05-27 17:56:40 +0200

    add slak

---
+++ configs/SLaK/upernet_convnext_tiny_512_80k_ade20k_ss.py
@@ -0,0 +1,68 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+_base_ = [
+    '../_base_/models/upernet_SLaK.py', '../_base_/datasets/ade20k.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
+]
+crop_size = (512, 512)
+
+checkpoint_file = '/projects/0/prjste21060/projects/LoRA_LK/transfer/convnext_tiny_Rep/51494713/LoRA_ERK_s1.5/120epochs/checkpoint-best.pth'
+
+model = dict(
+    backbone=dict(
+        type='ConvNeXt_Rep',
+        in_chans=3,
+        depths=[3, 3, 9, 3], 
+        dims=[96, 192, 384, 768], 
+        drop_path_rate=0.1,
+        layer_scale_init_value=1.0,
+        out_indices=[0, 1, 2, 3],
+        kernel_size=[51,49,47,13,5],
+        LoRA=True,
+        width_factor=1.5,
+    init_cfg=dict(type='Pretrained', checkpoint=checkpoint_file)
+    ),
+    decode_head=dict(
+        in_channels=[144, 288, 576, 1152],
+        num_classes=150,
+    ),
+    auxiliary_head=dict(
+        in_channels=576,
+        num_classes=150
+    ), 
+    test_cfg = dict(mode='slide', crop_size=crop_size, stride=(341, 341)),
+)
+
+optimizer = dict(constructor='LearningRateDecayOptimizerConstructor', _delete_=True, type='AdamW', 
+                 lr=0.0001, betas=(0.9, 0.999), weight_decay=0.05,
+                 paramwise_cfg={'decay_rate': 0.9,
+                                'decay_type': 'stage_wise',
+                                'num_layers': 6})
+
+lr_config = dict(_delete_=True, policy='poly',
+                 warmup='linear',
+                 warmup_iters=1500,
+                 warmup_ratio=1e-6,
+                 power=1.0, min_lr=0.0, by_epoch=False)
+
+# By default, models are trained on 8 GPUs with 2 images per GPU
+data=dict(samples_per_gpu=8)
+
+runner = dict(type='IterBasedRunnerAmp')
+
+# do not use mmdet version fp16
+fp16 = None
+optimizer_config = dict(
+    type="DistOptimizerHook",
+    update_interval=1,
+    grad_clip=None,
+    coalesce=True,
+    bucket_size_mb=-1,
+    use_fp16=True,
+)
