9d05d6016a8b46d03bdda56e8909ce801fa5e2f0
Shiweiliuiiiiiii <s.liu3@tue.nl>
2022-05-27 18:33:58 +0200

    add slak

--- mmseg/models/backbones/slak.py
+++ mmseg/models/backbones/slak.py
@@ -6,6 +6,7 @@
 # LICENSE file in the root directory of this source tree.
 import sys, os
 sys.path.append('/home/shiweil/Projects/cutlass/examples/19_large_depthwise_conv2d_torch_extension')
+sys.path.append('../../../')
 import warnings
 from collections import OrderedDict
 import torch
@@ -16,6 +17,7 @@ from timm.models.registry import register_model
 from depthwise_conv2d_implicit_gemm import DepthWiseConv2dImplicitGEMM
 from mmseg.utils import get_root_logger
 from ..builder import BACKBONES
+import mmcv_custom
 from mmcv.runner import (BaseModule, CheckpointLoader, ModuleList,
                          load_state_dict)
 from functools import partial
